eec18f6be632fe3a7c5ec8b7580e4f46851e4564
Lior Ehrlich <liorehrlich@gmail.com>
2020-11-28 10:56:51 +0200

    add theming to app

--- src/components/common/Button.js
+++ src/components/common/Button.js
@@ -3,6 +3,10 @@ const { default: styled } = require("styled-components");
 const Button = styled.button`
   border-radius: 3px;
   padding: 5px;
+  background-color: ${props => props.theme.colors.tertiary};
+  font-weight: bold;
+  color: ${props => props.theme.colors.primaryBackground};
+  border: 2px solid ${props => props.theme.colors.secondary};
 
   :hover {
     cursor: pointer;
---
+++ src/theme.js
@@ -0,0 +1,12 @@
+const theme = {
+  colors: {
+    primaryBackground: "#FFFFFF",
+    secondaryBackground: "#F2F2F2",
+    primary: "#F2CB05",
+    primaryContrast: "#79C4F2",
+    secondary: "#F2BF27",
+    tertiary: "#D99441"
+  }
+}
+
+export default theme
